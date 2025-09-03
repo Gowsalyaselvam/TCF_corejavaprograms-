67e8824dd5b93df9d4d94c68a3bae2d734d05c8b
User <User@DESKTOP-KEB27MS>
2025-09-03 21:01:02 +0530

    .

--- corejava/src/corejava/Demo.java
+++ corejava/src/corejava/Demo.java
@@ -3,7 +3,7 @@ package corejava;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.print("hello");
+		System.out.println("hello");
 
 	}
 
---
+++ corejava/src/corejava/typeconversion.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class typeconversion {
+
+	public static void main(String[] args) {
+		byte num1=127;
+		int num2=num1;
+		
+		int numb3=300;
+		byte num4=(byte)numb3;
+		System.out.println(num4);
+	}
+
+}
