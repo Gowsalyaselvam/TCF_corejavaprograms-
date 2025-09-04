d85de929bdb34db69813d9875f974a3e3c355660
User <User@DESKTOP-KEB27MS>
2025-09-04 10:16:10 +0530

    .

---
+++ corejava/src/com/tcf/oops/encapulation/Customerdemo.java
@@ -0,0 +1,18 @@
+package com.tcf.oops.encapulation;
+
+public class Customerdemo {
+ 
+	public static void main(String[] args) {
+	   Customerjava c1=new Customerjava();
+	   c1.setCid(101);
+	   c1.setName("sai");
+	   c1.setCity("pondy");
+	   System.out.println(c1);
+	   Customerjava c2=new Customerjava();
+	   c2.setCid(101);
+	   c2.setName("mala");
+	   c2.setCity("chennai");
+	   System.out.println(c2);
+	   
+	}
+}
